6e1c9c674f34d74d5d0e3dcec3302cb54498d042
unknown <sh2581@nyu.edu>
2019-04-01 00:05:20 -0400

    Added dependencies jQuery, popper, and bootstrap

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 const store = configureStore();
 
