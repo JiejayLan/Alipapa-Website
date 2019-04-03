b05560529c7f308b048e660704f1c2bc79e9a848
Nan <nanchen432@gmail.com>
2019-04-02 23:37:09 -0400

    changes to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV === 'test') {
 }
 
 module.exports = (env) => {
-  const isProduction = process.env.NODE_ENV === 'production' ? "warning" : false;
+  const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
