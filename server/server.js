079886c88111630c0da505bd28b6732a31b5bd79
jieJayLan <lanjie3458@gmail.com>
2019-04-04 10:12:47 -0400

    add watch and reload

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const reload = require('reload'), watch = require('watch');
+const reload_server = reload(app);
 const publicPath = path.join(__dirname, '..', 'public');
+watch.watchTree(__dirname + '/../src', function (f, curr, prev) {
+  reload_server.reload();
+});
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
@@ -11,5 +16,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up');
+  console.log('Server is up on', port);
 });
--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const  DashboardPage= () => (
   <div>
-    DashboardPage content
+    DashboardPage content1
   </div>
 );
 
