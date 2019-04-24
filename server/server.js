0443f634aa022dd5f969945b1e141821189e50de
Nan <nanchen432@gmail.com>
2019-04-24 14:18:07 -0400

    editProfilePage can now retrieve data

--- server/server.js
+++ server/server.js
@@ -26,9 +26,6 @@ app.post("/login",auth.login({firebase}));
 //delete a user
 app.post("/delete",auth.delete({firebase}));
 
-//retrieve user profile
-app.post("/profile", require('./controller/profile_controller.js')({firebase}));
-
 //a route to control all message request
 app.use("/message",message_controller(MESSAGE_SYSTEM(firebase)));
 app.use('/controllers/items/:id', require('./controller/item_page_controller.js')({ itemManager: ITEM_MANAGER, orderManager: ORDER_MANAGER}));
