78a90ebdf6ce58036b018a785dbb85ba881d9be8
jieJayLan <lanjie3458@gmail.com>
2019-04-30 21:22:46 -0400

    change all username in message to userID

--- server/service/messageManager.js
+++ server/service/messageManager.js
@@ -1,12 +1,43 @@
 
+
 module.exports = (firebase) => {
     //database instance
     let DATABASE = firebase.database;
 
+    //check and return userID based on username
+    let checkID = (username) => {
+        return new Promise((resolve, reject) => {
+            DATABASE.ref('/users')
+                .orderByChild('username')
+                .equalTo(username)
+                .on('value', (snapshot) => {
+                    //no such user and return null
+                    if(!snapshot.val())
+                        resolve(null);
+                    else{
+                        let userID = Object.keys(snapshot.val())[0];
+                        resolve(userID);
+                    }
+            })
+        })
+    }
+
+    //check and return username based on userID
+    let checkUsername = (userID) => {
+        console.log("userID",userID);
+        return new Promise((resolve, reject) => {
+            DATABASE.ref('/users'+'/'+userID)
+                .once('value')
+                .then((snapshot) => {
+                    let user = snapshot.val();
+                    resolve(user.username);
+            })
+        })
+    }
+
     const MESSAGE_SYSTEM = {
         /*send out a message
         data is an object, the key for diffenert message should be different
-        senderUserType will be removed in the future
         1.regular message
             messageType:"message"
             description
@@ -22,14 +53,14 @@ module.exports = (firebase) => {
             messageType:"complain"
             receiver:"SU"
             sender
-            compainedUsername:(who you want to complain)
+            compainUser:(who you want to complain)
             status:"suspended" or "justified"
         4.explain(OU only)
             description
             messageType:"explain"
             receiver:"SU"
             sender
-            explainUsername:(who you want to explain)
+            explainUser:(who you want to explain)
         5.appeal(OU only)    
             description
             messageType:"appeal"
@@ -37,7 +68,17 @@ module.exports = (firebase) => {
             sender         
         */
 
-        send: (data) => {
+        //send out a message
+        send: async (data) => {
+
+            //replace all username with userID
+            for(let key in data){
+                if(key === "sender" || key === "complaintUser" || key === "explainUser" ||key ==="receiver"){
+                    let userID = await checkID(data[key]);
+                    data[key] = userID;
+                }
+            }
+            
             return new Promise((resolve, reject) => {
                 (DATABASE.ref('/message').push().set({ ...data },
                     function (error) {
@@ -55,44 +96,59 @@ module.exports = (firebase) => {
         },
 
         //	check messages
-        checkReceiveMessage: (username, res) => {
+        checkReceiveMessage: (userID, res) => {
             DATABASE.ref('/message')
                 .orderByChild("receiver")
-                .equalTo(username)
-                .on('value', (snapshot) => {
-                    // console.log("snapshot",snapshot.val());  
-                    let message = snapshot.val();
-                    if (message === undefined) {
+                .equalTo(userID)
+                .on('value', async (snapshot) => {
+                    let messages = snapshot.val();
+
+                    //replace all userID with username
+                    for(let key in messages){
+                        let message = messages[key]
+                        let sender = await checkUsername(message.sender);
+                        message.sender = sender;
+                    }
+                        
+                    if (messages === undefined) {
                         res
                             .status(204)
-                            .json(message);
+                            .json(messages);
                         res.end();
                     }
                     else {
                         res
                             .status(200)
-                            .json(message);
+                            .json(messages);
                         res.end();
                     }
                 });
         },
         //	check received complain messages
-        checkReceiveComplain: (username, res) => {
+        checkReceiveComplain: (userID, res) => {
             DATABASE.ref('/message')
-                .orderByChild("complaintedUsername")
-                .equalTo(username)
-                .on('value', (snapshot) => {
-                    let message = snapshot.val();
-                    if (message === undefined) {
+                .orderByChild("complaintUser")
+                .equalTo(userID)
+                .on('value', async (snapshot) => {
+                    let messages = snapshot.val();
+
+                    //replace all userID with username
+                    for(let key in messages){
+                        let message = messages[key]
+                        let sender = await checkUsername(message.sender);
+                        message.sender = sender;
+                    }
+
+                    if (messages === undefined) {
                         res
                             .status(204)
-                            .json(message);
+                            .json(messages);
                         res.end();
                     }
                     else {
                         res
                             .status(200)
-                            .json(message);
+                            .json(messages);
                         res.end();
                     }
                 });
