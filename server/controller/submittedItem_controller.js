49d1bce4b81cbd125f6d76cd6fff3f9ce09328a1
Nan <nanchen432@gmail.com>
2019-05-01 23:09:17 -0400

    display keywords for every on-sell item

---
+++ server/controller/submittedItem_controller.js
@@ -0,0 +1,18 @@
+module.exports = (data) => {
+  let firebase = data.firebase;
+
+  return(req, res) => {
+    firebase.database.ref('/total_items').once('value').then((snapshot) => {
+      let totalItems = snapshot.val();
+      let onSellItems = [];
+      for (let itemID in totalItems){
+        if(totalItems[itemID]["seller"] === req.body.userID 
+          && totalItems[itemID]["status"] === "good"){
+            onSellItems.push({itemID, ...totalItems[itemID]});
+          }
+      }
+
+      res.json(onSellItems);
+    });
+  }
+} 
--- src/components/SubmittedItemApp.js
+++ src/components/SubmittedItemApp.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import axios from 'axios';
 
-class SubmittedItemApp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state={
-      
-    }
-  }
+const SubmittedItemApp = ({keywords}) => {
+  //change "to=" attribute to redirect to edit item form
   
-  render() {
+  const KEYWORDS = Object.keys(keywords);
   return (
-    <div>
-    <div className="list-header">
-      <h2 className="list-item__title">Submitted Item Applications</h2>
-    </div>
-
-    <div className="list-body">
-      <Link className="list-item--column">
-        <h3 className="list-item__title"></h3>
-        <h3 className="list-item__title"></h3>
-        <h3 className="list-item__title">
-          
-        </h3>
-        <h3 className="list-item__title"></h3>
-        <h3 className="list-item__title">
-        
-        </h3>
+      <Link className="list-item" to="/account">
+      <div>
+        <h3 className="list-item__subtitle">Keywords:</h3>
+        <h4 className="list-item__text">{KEYWORDS.join(", ")}</h4>
+       </div>
       </Link>
-    </div>
-  </div>
   );
-  }
 }
 
 export default SubmittedItemApp;
