6531ddd85e0d9b52c9a410587ec5c515dca25842
Nan <nanchen432@gmail.com>
2019-05-02 18:51:48 -0400

    SubmittedItemApp can dynamically retrieve data from firebase

--- src/components/SubmittedItemApp.js
+++ src/components/SubmittedItemApp.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const SubmittedItemApp = ({keywords}) => {
+const SubmittedItemApp = ({title, keywords}) => {
   //change "to=" attribute to redirect to edit item form
   
-  const KEYWORDS = Object.keys(keywords);
+  const KEYWORDS = keywords ?  Object.keys(keywords) : [''] ;
+  console.log(KEYWORDS);
   return (
-      <Link className="list-item" to="/account">
+      <Link className="list-item--column" to="/account">
+      <div>
+      <h3 className="list-item__subtitle">Title: {title}</h3>
+      </div>
+      
       <div>
         <h3 className="list-item__subtitle">Keywords:</h3>
         <h4 className="list-item__text">{KEYWORDS.join(", ")}</h4>
