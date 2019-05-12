1d506704cb865e2bf6ea7a9a18495663c716c6e1
RyanLinrm <runminlin@gmail.com>
2019-05-11 22:28:41 -0400

    click item redirect finished

--- src/components/homePage/ItemList.js
+++ src/components/homePage/ItemList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import * as firebase from "firebase";
 import { connect } from 'react-redux';
 import Hotness from './hotness';
+import {Link} from 'react-router-dom';
 
 class ItemList extends React.Component {
     constructor(props) {
@@ -38,19 +39,23 @@ class ItemList extends React.Component {
         Itemlist.sort( (a, b) => (a.hotness < b.hotness ) ? 1 : -1 );
 
         let jsxOUlist = Itemlist.map( (item) =>
-            <div className='col-9 mx-auto col-md-6 col-lg-3 my-3 rounded float-left' key={item.itemID}>
-                <div className='card'>
-                    <div className='img-container p-5'>
-                       <img src={item.url} className='card-img-top' alt='item'/>
-                    </div>
-                    <div className='card-footer d-flex justify-content-between'>
-                        <p className='align-self-center mb-0'>{item.title}</p>
-                        <h5 className='text-blue font-italic mb-0'><span>&#36;</span>{item.price.current}</h5>
-                    </div>
-                    <p className='float-left'>&nbsp;&nbsp;Hotness: <Hotness count={item.hotness}/></p>
-            </div>
+            <div key={item.itemID}>
+                <Link className="list-item__title--link" to={`/items/${item.itemID}`}>
+                    <div className='col-9 mx-auto col-md-6 col-lg-3 my-3 rounded float-left'>
+                        <div className='card'>
+                            <div className='img-container p-5'>
+                            <img src={item.url} className='card-img-top' alt='item'/>
+                            </div>
+                            <div className='card-footer d-flex justify-content-between'>
+                                <p className='align-self-center mb-0'>{item.title}</p>
+                                <h5 className='text-blue font-italic mb-0'><span>&#36;</span>{item.price.current}</h5>
+                            </div>
+                            <p className='float-left'>&nbsp;&nbsp;Hotness: <Hotness count={item.hotness}/></p>
+                        </div>
 
-        </div>
+                    </div>
+                </Link>
+            </div>
     )
 
         return jsxOUlist;
