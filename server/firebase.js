2064e69fb0004a268dbc7fa06173d2b8e99ddafe
Jie Lan <lanjie3458@gmail.com>
2019-07-02 19:10:14 -0400

    set up

--- server/firebase.js
+++ server/firebase.js
@@ -1,24 +1,18 @@
 var firebase = require("firebase");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyAptktEUMMsGMBe2Xkn8N-5C15Q5byy67o",
+  authDomain: "mini - ebymazon - test.firebaseapp.com",
+  databaseURL: "https://mini-ebymazon-test.firebaseio.com",
+  projectId: "mini - ebymazon - test",
+  storageBucket: "mini - ebymazon - test.appspot.com",
+  messagingSenderId: "53649890005"
 };
 
 firebase.initializeApp(config);
 
-
-
 module.exports.database = firebase.database();
 // module.exports.storage = firebase.storage();
 module.exports.googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-
-
-
-
